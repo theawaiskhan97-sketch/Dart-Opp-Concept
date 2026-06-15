2532580356bc1f6e1cc04ca635fefb073cbb67a4
Awais Khan <theawaiskhan97@gmail.com>
2026-06-15 15:42:13 +0500

    Update introoop.dart

--- introoop.dart
+++ introoop.dart
@@ -2,16 +2,18 @@ class Student {
   String name;
   int age;
   String city;
+  String job;
   // constructor
-  Student(this.name, this.age, this.city);
+  Student(this.name, this.age, this.city, this.job);
   void display() {
     print("Name is : $name");
     print("Age is: $age");
     print("City is: $city");
+    print("Job Status: $job");
   }
 }
 
 void main() {
-  Student s1 = Student("Ali", 20, "Karachi");
+  Student s1 = Student("Ali", 20, "Karachi", "Engineer");
   s1.display();
 }
